001b26ad7a7fbd713c762f646d31665801956c91
talktokris <talktokris@gmail.com>
2023-02-24 18:56:01 +0545

    laravel vue basic setup done and working 2

---
+++ resources/js/components/TestComponent.vue
@@ -0,0 +1,28 @@
+<script>
+
+const Kris= ["one", "two", "three", "Four"];
+
+export default {
+
+    data() {
+    return {
+      message: Kris,
+      isRed: true,
+      color: 'green'
+    }
+  }
+}
+</script>
+
+<template lang="">
+    <div>
+       {{ message[0]}}
+    </div>
+</template>
+
+
+
+
+<style lang="">
+    
+</style>
